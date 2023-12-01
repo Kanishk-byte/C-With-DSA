770662a0a91fd505f98d2a21cbf12b4af7f3346f
Kanishk Khandelwal <khandelwalkanishk07@gmail.com>
2023-12-01 21:45:20 +0530

    Files

---
+++ Diving More into C++ Programming (Week - 4)/Compilation Process (Lecture - 3)/Programs/demo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// * Demo File :-
+
+int main()
+{
+    cout << "Hello World!!";
+    return 0;
+}
---
+++ Diving More into C++ Programming (Week - 4)/Compilation Process (Lecture - 3)/Programs/shortLong.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#define LN long
+
+// * Short Long Replacement Method -> Valid for all Datatypes.
+
+int main()
+{
+
+    LN x;
+    x = 8875549179;
+    cout<<x;
+
+    return 0;
+}
